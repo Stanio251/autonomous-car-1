2bcc8904f933381175710c947ddfb08c293fc90c
Jakub <Stanio251>
2025-01-27 14:46:57 +0100

    some beginner tutorials on ros2

---
+++ build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jakub/programming/nxp_car/src/cpp_pubsub
+# Build directory: /home/jakub/programming/nxp_car/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
