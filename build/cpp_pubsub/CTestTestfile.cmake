6917756c06e45cd033c57bc3398b052519a65d28
Jakub <Stanio251>
2025-02-03 12:40:05 +0100

    removed build, log, install folders (obsolete in git)

--- build/cpp_pubsub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jakub/programming/nxp_car/src/cpp_pubsub
-# Build directory: /home/jakub/programming/nxp_car/build/cpp_pubsub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
